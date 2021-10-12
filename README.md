60c898e0fcce99d00d5326be43eb37e82c68b3c5
Amrish Bharatiya <bamrish@gmail.com>
2021-10-13 00:05:55 +0530

    intial code for svg play

---
+++ README.md
@@ -0,0 +1,6 @@
+# SVG Play
+
+A simple program to try out svg.
+This is a bouncy ball animation created in SVG.
+
+
---
+++ main.js
@@ -0,0 +1,168 @@
+// variable for the namespace
+class Circle {
+    svgns = "http://www.w3.org/2000/svg";
+
+    cx = 0;
+    cy = 0;
+    r = 0;
+    node = null;
+    options = {
+        fill: 'black',
+        stroke: 'black',
+        strokeWidth: 1
+    }
+
+    constructor(x, y, radius, opts = {}) {
+        this.cx = x;
+        this.cy = y;
+        this.r = radius;
+        Object.assign(this.options, opts);
+        this.#create();
+    }
+
+    #create() {
+        this.node = document.createElementNS(this.svgns, "circle");
+        this.node.setAttribute("cx", this.cx);
+        this.node.setAttribute("cy", this.cy);
+        this.node.setAttribute("r", this.r);
+        this.#applyOptions();
+        return this.node;
+    }
+
+    #applyOptions() {
+        this.node.setAttribute("fill", this.options.fill);
+        this.node.setAttribute("stroke", this.options.stroke);
+        this.node.setAttribute("strokeWidth", this.options.strokeWidth);
+    }
+
+    getNode() {
+        return this.node;
+    }
+
+    x(val) {
+        if (!val) {
+            return this.cx;
+        }
+        this.cx = val;
+        this.node.setAttribute("cx", this.cx);
+        return this;
+    }
+
+    y(val) {
+        if (!val) {
+            return this.cy;
+        }
+        this.cy = val;
+        this.node.setAttribute("cy", this.cy);
+        return this;
+    }
+
+    radius(val) {
+        if (!val) {
+            return this.r;
+        }
+        this.r = val;
+        this.node.setAttribute("r", this.r);
+        return this;
+    }
+
+    opts(val) {
+        if (!val) {
+            return this.options;
+        }
+        Object.assign(this.options, val || {});
+        this.#applyOptions();
+    }
+
+    toString() {
+        return `x = ${this.cx}, y = ${this.cy}, r = ${this.r};`;
+    }
+
+}
+
+class Animate {
+    static CIRCLE_RADIUS = 30;
+    static CIRCLE_X_START_OFFSET = 1;
+    static CIRCLE_Y_START_OFFSET = 1;
+    static SPEED = 2;
+    static START_X = (Animate.CIRCLE_RADIUS * 2 + Animate.CIRCLE_X_START_OFFSET);
+    static START_Y = (Animate.CIRCLE_RADIUS * 2 + Animate.CIRCLE_Y_START_OFFSET);
+    static START_OPTIONS = {
+        fill: 'yellow',
+        stroke: 'yellow'
+    };
+
+    svg = null;
+    xSpeed = Animate.SPEED;
+    ySpeed = Animate.SPEED;
+    interval = 0;
+    count = 0;
+    circle;
+
+    constructor(svg) {
+        this.svg = svg;
+        this.circle = new Circle(Animate.START_X, Animate.START_Y, Animate.CIRCLE_RADIUS, Animate.START_OPTIONS);
+        this.svg.appendChild(this.circle.getNode());
+    }
+
+    start() {
+        this.interval = setInterval(() => {
+            this.moveCircle()
+        }, 1);
+    }
+
+    moveCircle() {
+        const A = Animate;
+        const c = this.circle;
+        const xLowerLimit = A.CIRCLE_RADIUS;
+        const yLowerLimit = A.CIRCLE_RADIUS;
+        const xUpperLimit = this.svg.clientWidth - A.CIRCLE_RADIUS - A.CIRCLE_X_START_OFFSET;
+        const yUpperLimit = this.svg.clientHeight - A.CIRCLE_RADIUS - A.CIRCLE_Y_START_OFFSET;
+
+        if (c.x() <= xLowerLimit || c.x() >= xUpperLimit) {
+            this.xSpeed = this.xSpeed * -1;
+            const color = this.randomColor();
+            c.opts({fill: color, stroke: color});
+        }
+
+        if (c.y() <= yLowerLimit || c.y() >= yUpperLimit) {
+            this.ySpeed = this.ySpeed * -1;
+            const color = this.randomColor();
+            c.opts({fill: color, stroke: color});
+
+        }
+
+        c.x(c.x() + this.xSpeed)
+        c.y(c.y() + this.ySpeed)
+    }
+
+    stop() {
+        clearInterval(this.interval);
+    }
+
+    randomColor() {
+        const r = Math.abs(Math.random() * 255);
+        const g = Math.abs(Math.random() * 255);
+        const b = Math.abs(Math.random() * 255);
+        return `rgb(${r}, ${g}, ${b})`
+    }
+}
+let started = false;
+let animate = null;
+
+window.addEventListener('click', function (){
+    if(animate && !started) {
+        animate.start();
+        started = true;
+    } else if(animate && started) {
+        animate.stop();
+        started = false;
+    }
+});
+
+window.addEventListener('load', () => {
+    const svg = document.getElementById("svg");
+    animate = new Animate(svg);
+});
+
+
